5a1863642e434ca1d930a86fba90047536934db3
milagrosMndoza <milagrosmndoza@gmail.com>
2017-12-04 11:22:19 -0500

    subiendo mi portafolio

---
+++ portafolio.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top,
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
+  //Project hover animation 
+  $('#portfolio .thumbnail').hover(function(){
+    $(this).children(".cust-caption").slideDown();
+  },
+  function(){
+    $(this).children(".cust-caption").fadeOut();
+  });
+});
+  
+ //Skills' animation
+$(window).scroll(function(){
+  jQuery('.skillbar').each(function(){
+			jQuery(this).find('.skillbar-bar').animate({
+				width:jQuery(this).attr('data-percent')
+			},6000);
+		});
+});
+
+$(document).ready();
+
