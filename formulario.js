c9ab963118c5b4551ab337b1f015a8e35e68637b
milagrosMndoza <milagrosmndoza@gmail.com>
2017-12-21 08:51:29 -0500

    conectando mi firebase con mi formulario al 100%

--- formulario.js
+++ formulario.js
@@ -1,65 +1,30 @@
-// 	  // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyApDvbBTiKOrqrB4BqMsMpwL8C_ooawXOE",
-  authDomain: "bd-portafolio.firebaseapp.com",
-  databaseURL: "https://bd-portafolio.firebaseio.com",
-  projectId: "bd-portafolio",
-  storageBucket: "bd-portafolio.appspot.com",
-  messagingSenderId: "783955591124"
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyApDvbBTiKOrqrB4BqMsMpwL8C_ooawXOE",
+    authDomain: "bd-portafolio.firebaseapp.com",
+    databaseURL: "https://bd-portafolio.firebaseio.com",
+    projectId: "bd-portafolio",
+    storageBucket: "",
+    messagingSenderId: "783955591124"
   };
   firebase.initializeApp(config);
 
-//       // Reference messages collection
-// var messagesRef = firebase.database().ref('messages');
-
-// // Listen for form submit
-// document.getElementById('contactForm').addEventListener('submit', submitForm);
-
-// // Submit form
-// function submitForm(e){
-//   e.preventDefault();
-
-//   // Get values
-//   var name = getInputVal('name');
-//   var email = getInputVal('email');
-//   var phone =getInputVal('cel');
-//   var message = getInputVal('message');
-
-//   // Save message
-//   saveMessage(name, email, cel, message);
-
-//   // Show alert
-//   document.querySelector('.alert').style.display = 'block';
-
-//   // Hide alert after 3 seconds
-//   setTimeout(function(){
-//     document.querySelector('.alert').style.display = 'none';
-//   },3000);
-
-//   // Clear form
-//   document.getElementById('contactForm').reset();
-// }
-// // Function to get get form values
-// function getInputVal(id){
-//   return document.getElementById(id).value;
-// }
-
-// // Save message to firebase
-// function saveMessage(name, email, cel, message){
-//   var newMessageRef = messagesRef.push();
-//   newMessageRef.set({
-//    name, email, cel, message
-//   });
-// }
 $('#contactForm').submit(saveData);
 function saveData(event) {
-let name = $('#name').val();
+ 
+let name = $('#input-name').val();
 let email = $('#email').val();
-let cel = $('#input-cel').val();
+let cel = $('#cel').val();
 let message = $('#message').val();
-event.preventDefault();
-console.log('datos',name,email, cel, message);
+
+console.log('datos',name, email, cel, message);
 writeUserData(name, email, cel, message)
+// $('#contactForm').trigger("reset");
+$('#input-name').val("");
+$('#email').val("");
+$('#cel').val("");
+$('#message').val("");
+event.preventDefault();
 }
 
 function writeUserData(name, email, cel, message) {
@@ -69,4 +34,6 @@ firebase.database().ref('users/').push({
   cel:cel,
   message:message
 });
+
+
 }
